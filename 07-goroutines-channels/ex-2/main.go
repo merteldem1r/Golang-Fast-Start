9145ed37107e0ecbf5a5c8bb2c20426c8d8a1d71
Mert Eldemir <mert1greatest@gmail.com>
2026-02-09 11:42:28 +0300

    goroutines & channels updates

--- 07-goroutines-channels/ex-2/main.go
+++ 07-goroutines-channels/ex-2/main.go
@@ -1,86 +1,31 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/joho/godotenv"
 )
 
-// Get API Key from the https://openweathermap.org/ and locate in .env file
-// http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}
-
-type WeatherResponse struct {
-	Main struct {
-		Temp float64 `json:"temp"`
-	} `json:"main"`
-	Name string `json:"name"`
-}
-
-type WeatherResult struct {
-	Data WeatherResponse
-	Err  error
+func someFunc(num string) {
+	fmt.Println(num)
 }
 
 func main() {
-	godotenv.Load()
-	apiKey := os.Getenv("OPENWEATHER_API_KEY")
+	fmt.Println("Goroutines and Channels")
 
-	if apiKey == "" {
-		fmt.Printf("Can not get API KEY")
-		return
+	ch := make(chan string)
+	ch2 := make(chan string)
+
+	go func() {
+		ch <- "data"
+	}()
+
+	go func() {
+		ch2 <- "data2"
+	}()
+
+	select {
+	case msgFromCh := <-ch:
+		fmt.Println(msgFromCh)
+	case msgFromCh2 := <-ch2:
+		fmt.Println(msgFromCh2)
 	}
-
-	cities := []string{"Toronto", "London", "Paris", "Tokyo", "Istanbul", "Moscow", "Oslo", "Ankara"}
-
-	ch := make(chan WeatherResult)
-
-	startTime := time.Now()
-
-	// Launch all goroutines
-	for _, city := range cities {
-		go fetchWeather(city, apiKey, ch)
-	}
-
-	// Collect results (one receive per goroutine)
-	for range cities {
-		result := <-ch
-		if result.Err != nil {
-			fmt.Println("Error:", result.Err)
-			continue
-		}
-		fmt.Printf("City: %v, Temperature: %v\n", result.Data.Name, result.Data.Main.Temp)
-	}
-
-	fmt.Printf("Time taken to fetch all cities: %v", time.Since(startTime))
-}
-
-func fetchWeather(city string, apiKey string, ch chan<- WeatherResult) {
-	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apiKey)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		ch <- WeatherResult{Err: err} // send error through channel
-		return
-	}
-
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		ch <- WeatherResult{Err: err}
-		return
-	}
-
-	var data WeatherResponse
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		ch <- WeatherResult{Err: err}
-		return
-	}
-
-	ch <- WeatherResult{Data: data} // send success through channel
 }
